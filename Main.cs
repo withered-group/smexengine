e08c6cd5abac06eba4c60bdae28c24377728859f
Pixelwarp <marco11@tutanota.com>
2022-09-08 20:27:27 +0200

    first
    just first

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmexEngine {
+  class MainProgram {
+
+    // CHANGE WHEN NOT IN DEV ENV
+    bool dev = true;
+
+    static void Init() {
+      Console.WriteLine(value: "[SMEX] Attempting to start...");
+      // Do startup actions
+      // Keyboard, graphics, etc
+    }
+
+    static void Main(string[] args) {
+      Init();
+    }
+  }
+}
