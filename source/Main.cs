94bae60bb5470fb0f163393204704df75ed96318
foxl <every.flipped@gmail.com>
2022-09-08 20:15:27 +0100

    you're mom

--- source/Main.cs
+++ source/Main.cs
@@ -4,15 +4,21 @@ namespace SmexEngine {
   class MainProgram {
 
     // CHANGE WHEN NOT IN DEV ENV
-    bool dev = true;
+    static bool dev = false;
 
     static void Init() {
-      Console.WriteLine(value: "[SMEX] Attempting to start...");
+      if (dev) Console.WriteLine(value: "[SMEX] Attempting to start in development state...");
+      else Console.WriteLine(value: "[SMEX] Attempting to start...");
       // Do startup actions
       // Keyboard, graphics, etc
     }
 
     static void Main(string[] args) {
+      if (!(args.Count() == 0)) {
+        if (Array.IndexOf(args, "--dev") > -1) {
+          dev = true;
+        }
+      }
       Init();
     }
   }
